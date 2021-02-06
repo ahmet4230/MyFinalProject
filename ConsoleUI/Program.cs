1591367d2b2d9954da705e752c543c8f090ec6b5
ahmet batman <ahmet_batman123@hotmail.com>
2021-02-07 00:14:41 +0300

    7.02.2021 Güncel.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,23 +12,28 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            //ProductTest();
+            ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
             CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
             foreach (var category in categoryManager.GetAll())
             {
                 Console.WriteLine(category.CategoryName);
-               
-            }
 
+            }
         }
 
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetAllByCategoryId(10))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine("Ürün İsmi: " + product.ProductName + " / " + "Kategori ismi: " +  product.CategoryName );
             }
             
         }
