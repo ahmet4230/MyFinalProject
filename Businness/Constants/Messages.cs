5c3008b80b57c9ff6de3c974a674414bf1d15f0b
ahmet batman <ahmet_batman123@hotmail.com>
2021-02-11 00:15:08 +0300

    11.02.2021 Ders sonu.

---
+++ Businness/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Businness.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün eklendi";
+        public static string ProductNameInvalild = "Ürün ismi geçersiz";
+        public static string MaintenanceTime = "Sistem bakımda";
+        public static string ProductsListed = "Ürünler listelendi";
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -31,10 +31,23 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success==true)
             {
-                Console.WriteLine("Ürün İsmi: " + product.ProductName + " / " + "Kategori ismi: " +  product.CategoryName );
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine("Ürün İsmi: " + product.ProductName + " / " + "Kategori ismi: " + product.CategoryName);
+                }
+
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+
+            
             
         }
 
